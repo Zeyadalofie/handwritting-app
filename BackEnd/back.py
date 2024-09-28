1e9bf7e3d2ed9226d21097acc9893dcd2aa0c0ca
Zeyad <109035553+Zeyadalofie@users.noreply.github.com>
2024-09-28 12:32:00 +0300

    Committing all changes

---
+++ BackEnd/back.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonfly
+from PIL import Image
+from tensorflow.python.keras.models import load_model
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    if 'image' not in request.files:
+        return jsonfly({'error' : 'No image uploaded'}), 400
+    
+    file = request.files['image']
+    img = Image.open(file.stream)
+
+
+    model = load_model('./TheGoodHW_version/HandWriting_model.h5')
+    prediction = model.predict(img)
+
+
+    return jsonfly({'prediction' : 'handwritten text'})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
